cc98d8d023f7a5fe4b81b0dcb46142cfc0388dc9
Arun Kumar <java.kumar.arun@gmail.com>
2026-06-08 16:46:36 +0530

    8June - JS started

---
+++ 3_js/d1_datatype.js
@@ -0,0 +1,18 @@
+let x = 3;
+typeof(x);
+
+x = true;
+typeof(x);
+
+x = function(){};
+typeof(x);
+
+x = "string";
+typeof(x);
+
+x = {};
+typeof(x);
+
+
+
+
---
+++ 3_js/d2_nested_if.js
@@ -0,0 +1,16 @@
+// apply for license - use valid 
+
+max = 75
+min = 18
+
+let age = 78;
+
+if(age >= min){
+    if(age <= max){
+        console.log("Valid age");
+    } else {
+        console.log("Old age");
+    }
+} else {
+    console.log("Minor age");
+}
